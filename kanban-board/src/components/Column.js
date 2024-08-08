14cf1684fda411874fdda48818a49441914adab5
Ananya Yadav <ananyayadav@Ananyas-MacBook-Air.local>
2024-08-08 16:48:59 +0530

    Initial commit

---
+++ kanban-board/src/components/Column.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Task from './Task';
+
+const Column = ({ column, emptyTrash }) => (
+  <li className={`column ${column.id}-column`}>
+    <div className="column-header">
+      <h4>{column.title}</h4>
+    </div>
+    <ul className="task-list" id={column.id}>
+      {column.tasks.map((task, index) => (
+        <Task key={index} task={task} />
+      ))}
+    </ul>
+    {column.id === 'trash' && (
+      <div className="column-button">
+        <button className="button delete-button" onClick={emptyTrash}>Delete</button>
+      </div>
+    )}
+  </li>
+);
+
+export default Column;
---
+++ kanban-board/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Drag & Drop<br /><span>Lean Kanban Board</span></h1>
+  </header>
+);
+
+export default Header;
